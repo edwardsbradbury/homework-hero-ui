ebba2bd6f64bd235de3668d1334e3992e4ff669b
Ed Bradbury <ebrad005@gold.ac.uk>
2022-03-14 11:32:48 +0000

    I believe Onboarding is now functioning as intended - appears to be at least.

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -6,7 +6,7 @@ import API from '../features/API';
 function Onboarding() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.value);
-  const userType = useState(user.type);
+  const userType = user.type;
 
   const messageIfClient = useState(`Add a subject that you need help with and the level you're studying this subject at. This will help tutors who teach that subject to find you`);
   const messageIfTutor = useState(`Choose a subject you want to teach and the level you're confident teaching this subject at`);
@@ -30,7 +30,7 @@ function Onboarding() {
           id: user.id,
           first: user.forename,
           last: user.lastname,
-          userType,
+          userType: userType,
           subject: subject,
           level: level
         }
