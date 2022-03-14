a9da077a745b93718332d6601e1d98627bbe6a15
Ed Bradbury <ebrad005@gold.ac.uk>
2022-03-14 10:34:37 +0000

    Logging response from add subject API route

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -35,6 +35,7 @@ function Onboarding() {
           level: level
         }
       ))
+      console.log(result);
       // API().post('add_subject', {
       //   id: user.id,
       //   first: user.forename,
