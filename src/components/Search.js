a38bb3c19107572b01b0d16214892350ce03d769
Ed Bradbury <ebrad005@gold.ac.uk>
2022-03-15 20:17:10 +0000

    Eventually figured out I was getting multiple SearchForms instead of SearchResult components because I'd inadvertently imported SearchForm as alias SearchResult on line 4. Because I'd copy and pasted line 3 and forgot to change the file path to SearchResults.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import SearchForm from './SearchForm';
-import SearchResult from './SearchForm';
+import SearchResult from './SearchResults';
 
 function Search() {
   const mode = useSelector((state) => state.search.value.mode);
@@ -12,7 +12,8 @@ function Search() {
     <div id='search'>
       <h1>Search for {userType === 'client' ? 'tutors' : 'students'}</h1>
       <SearchForm />
-      {results.length > 0 && results.map((result) => <SearchResult key={result.id} resultData={result}/>)}
+      {(results.length > 0) && results.map((result) => <SearchResult key={result.id} resultData={result}/>)}
+      {/* {results.length > 0 && <SearchResults />} */}
     </div>
   )
 }
