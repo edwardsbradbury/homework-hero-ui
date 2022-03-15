236f6b4588c3deb5964c5799db58925dbd37009d
Ed Bradbury <ebrad005@gold.ac.uk>
2022-03-15 15:21:25 +0000

    Some console logging statements  in SearchForm, lines 44 and 45 -  I can see in Redux devtools that the API response has a result property which is an object of user data, but the setResponse action is being dispatched with no argument. Also amended the argument to be an object with a property results with value result.result.values().

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -40,10 +40,10 @@ function SearchForm() {
             if (typeof result.result === 'string') {
               setError('No results matched your search criteria');
             } else {
-              // console.log('Setting search results');
-              // console.log(result.result);
-              // console.log(typeof result.result);
-              dispatch(setResults(result.result.values()));
+              // console.log(result);
+              console.log(typeof result.result);
+              console.log(result.result);
+              dispatch(setResults({results: result.result.values()}));
             }
           }
         })
