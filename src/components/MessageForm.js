287274c125ffac1db7a697d02c3729425d058534
Ed Bradbury <ebrad005@gold.ac.uk>
2022-03-23 23:06:40 +0000

    Scaffolded basic MessageForm component with textarea, send button, labels

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,11 +1,23 @@
-import React from 'react';
+// Component for writing new messages, with method to validate and send the message
+
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {sendMessage} from '../features/messaging';
 
 function MessageForm () {
+  const dispatch = useDispatch();
+  const [content, setContent] = useState('');
+  let charsRemaining = 500 - content.length;
+
+  function sendIt() {
+    
+  }
+
   return (
     <div id='messageForm'>
-
+      <textarea maxlength='500' placeholder='Write your message here...' onChange={e => setContent(e.target.value.trim())}></textarea>
+      <p>{charsRemaining}/500 characters remaining</p>
+      <button onclick={sendIt}>Send</button>
     </div>
   )
 }
