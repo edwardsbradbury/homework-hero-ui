b26744bed2032ccadf684bf97a1ff02195b6a020
Ed Bradbury <ebrad005@gold.ac.uk>
2022-04-08 09:48:36 +0100

    Conversation component - conditional rendering didn't work, maybe because was trying to render multiple components at same level. Tried creating variables renderifInbox and renderIfMessages, each with a div containing the necessary elements.

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -15,21 +15,31 @@ function Conversation (props) {
   const firstMessage = useState(props.messages[0]);
   const recipient = useState(firstMessage.senderId === user.id ? firstMessage.recipId : firstMessage.senderId);
   const messages = props.messages.map(aMessage => <Message key={aMessage.id} parentMode={mode} data={aMessage} userId={user.id}/>);
-  const displayInfo = (
-    <table>
-      <tr>
-        <td>{`Conversation with user: ${recipient}`}</td>
-        <td>{firstMessage.sent}</td>
-      </tr>
-    </table>);
   const generalError = useState('Failed to get messages');
   const confirmDeletion = useState('Are you sure you want to delete this message?');
   const deletionFailed = useState('Failed to delete your message');
+  const renderIfInbox = (
+    <div>
+      <h3>{`Chat with user: ${recipient}`}</h3>
+      <br />
+      <Message parentMode={mode} data={firstMessage} userId={user.id}/>
+    </div>
+  );
+  const renderIfMessages = (
+    <div>
+      <h3>{`Chat with user: ${recipient}`}</h3>
+      <br />
+      <MessageForm convId={firstMessage.convId} recipient={recipient} setErrors={props.setErrors} />
+      {messages}
+    </div>
+  );
 
   return (
     <div className='conversation'>
-      {mode === 'inbox' && displayInfo && <Message parentMode={mode} data={firstMessage} userId={user.id}/>}
-      {mode === 'messages' && displayInfo && <MessageForm convId={firstMessage.convId} recipient={recipient} setErrors={props.setErrors} /> && messages}
+      {/* {mode === 'inbox' && <Message parentMode={mode} data={firstMessage} userId={user.id}/>}
+      {mode === 'messages' && <MessageForm convId={firstMessage.convId} recipient={recipient} setErrors={props.setErrors} /> && messages} */}
+      {mode === 'inbox' && renderIfInbox}
+      {mode === '' && renderIfMessages}
     </div>
   )
 }
