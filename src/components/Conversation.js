fefb92c44377c1e515b89ec0eb5378cf692cd853
Ed Bradbury <ebrad005@gold.ac.uk>
2022-04-08 13:52:03 +0100

    Efforts to determine why recipient isn't being set in Conversations component

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -13,7 +13,8 @@ function Conversation (props) {
   const mode = useState('inbox');
   const user = useState(useSelector(state => state.user.value));
   const firstMessage = useState(props.messages[0]);
-  const recipient = useState(firstMessage.senderId === user.id ? firstMessage.recipId : firstMessage.senderId);
+  // const recipient = useState(firstMessage.senderId === user.id ? firstMessage.recipId : firstMessage.senderId);
+  const recipient = useState(props.messages[0].senderId === user.id ? props.messages[0].recipId : props.messages[0].senderId);
   const messages = props.messages.map(aMessage => <Message key={aMessage.id} parentMode={mode} data={aMessage} userId={user.id}/>);
   const generalError = useState('Failed to get messages');
   const confirmDeletion = useState('Are you sure you want to delete this message?');
@@ -39,7 +40,7 @@ function Conversation (props) {
       {/* {mode === 'inbox' && <Message parentMode={mode} data={firstMessage} userId={user.id}/>}
       {mode === 'messages' && <MessageForm convId={firstMessage.convId} recipient={recipient} setErrors={props.setErrors} /> && messages} */}
       {mode === 'inbox' && renderIfInbox}
-      {mode === '' && renderIfMessages}
+      {mode === 'messages' && renderIfMessages}
     </div>
   )
 }
