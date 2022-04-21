c898d775f4858d8467915c51d0c70b9e2e89d6f2
Ed Bradbury <ebrad005@gold.ac.uk>
2022-04-21 15:47:22 +0100

    Debugging - log in features/search line 40. Error in browser console that response is undefined

--- src/features/search.js
+++ src/features/search.js
@@ -37,6 +37,7 @@ export const searchReducer = createSlice({
   extraReducers: builder => {
     builder.addCase(getAllUsers.fulfilled, (state, action) => {
       const response = action.payload.data;
+      console.log(response);
       if (response.outcome === 'success') {
         state.value.allUsers = response.users;
         state.value.errors = [];
